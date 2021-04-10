9559fbb613a3d625bbc630ea05192e92414ab0a3
logotip4ik <bogdankostyuk12@gmail.com>
2021-04-10 21:57:39 +0300

    update: firts part of job complete. main layout, theme, fetch functions

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="nav">
+    <h1 class="nav__heading">HackerNews Clone</h1>
+  </nav>
+</template>
+
+<style lang="scss">
+.nav {
+  width: 100%;
+  padding: 0.5rem 1rem;
+  background-color: rgb(39, 41, 51);
+  box-shadow: 0 10px 10px 0 rgba($color: #000000, $alpha: 0.1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &__heading {
+    font-size: 2rem;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,37 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">hackernews-clone</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="main">
+    <Navbar />
+    <p v-if="$fetchState.pending">loading...</p>
+    {{ posts }}
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    posts: [],
+  }),
+  async fetch() {
+    const { data } = await this.$axios.get(
+      'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+    )
+
+    for (let i = 0; i < data.slice(0, 10).length; i += 1) {
+      const post = await this.$axios.get(
+        `https://hacker-news.firebaseio.com/v0/item/${data[i]}.json?print=pretty`
+      )
+      this.posts.push(post.data)
+    }
+  },
+  // Do i really need this?
+  fetchOnServer: false,
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
+<style lang="scss">
+.main {
   min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  background-color: rgb(31, 33, 41);
+  color: #aaa;
 }
 </style>
