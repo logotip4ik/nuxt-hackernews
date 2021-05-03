82f1de37891dc89503927e1bef7f3886c8ff7328
logotip4ik <bogdankostyuk12@gmail.com>
2021-05-03 20:20:15 +0300

    update: separated svg's and added offline popup

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,38 +9,20 @@
       }"
       @click="toggleDarkMode"
     >
-      <svg
-        v-if="darkMode"
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-sun"
-        viewBox="0 0 16 16"
-      >
-        <path
-          d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"
-        />
-      </svg>
-      <svg
-        v-else
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-moon"
-        viewBox="0 0 16 16"
-      >
-        <path
-          d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"
-        />
-      </svg>
+      <img v-if="darkMode" src="~/assets/sun.svg" alt="sun icon" />
+      <img v-else src="~/assets/moon.svg" alt="moon icon" />
     </button>
+    <transition :css="false" @enter="enter" @leave="leave">
+      <div v-if="$nuxt.isOffline" class="popup">
+        <span>You are offline</span>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
+import gsap from 'gsap'
 
 export default {
   computed: {
@@ -58,6 +40,15 @@ export default {
   },
   methods: {
     ...mapActions(['initDB', 'toggleDarkMode']),
+    enter(el, onComplete) {
+      gsap.fromTo(el, { translateY: '100%' }, { translateY: '0', onComplete })
+    },
+    leave(el, onComplete) {
+      gsap.to(el, { translateY: '100%', onComplete })
+    },
+    refresh() {
+      window.location.reload()
+    },
   },
 }
 </script>
@@ -109,18 +100,79 @@ body {
   }
 
   &--dark {
-    color: #aaa;
     border-color: #333;
     background-color: #1f2129;
 
     &:hover {
-      color: white;
       background-color: lighten($color: #1f2129, $amount: 10);
     }
   }
 
-  svg {
+  img {
     transform: scale(1.5);
   }
+}
+
+.popup {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #79ffe1;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  color: rgb(49, 49, 49);
+
+  & > * {
+    margin: 0.3rem 0.5rem;
+  }
+
+  button {
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.25rem;
+    background-color: #79ffe1;
+    border: none;
+    display: flex;
+    font: inherit;
+    color: currentColor;
+    position: relative;
+    cursor: pointer;
+    z-index: 1;
+    img {
+      transform: translateY(0.25px);
+      margin-right: 0.25rem;
+    }
+
+    &::after {
+      z-index: -1;
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba($color: #000000, $alpha: 0.15);
+      border-radius: 0.25rem;
+      opacity: 0;
+      transition: opacity 200ms ease-out;
+    }
+
+    &:hover::after {
+      opacity: 1;
+    }
+  }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
--- pages/s/_stories/_page.vue
+++ pages/s/_stories/_page.vue
@@ -1,23 +1,11 @@
 <template>
   <div ref="posts" :class="{ main: true, 'main--dark': darkMode }">
     <div ref="loader" :class="{ loader: true, 'loader--dark': darkMode }">
-      <svg
+      <img
         ref="loaderArrow"
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-arrow-clockwise"
-        viewBox="0 0 16 16"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"
-        />
-        <path
-          d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"
-        />
-      </svg>
+        src="~/assets/circle-arrow.svg"
+        alt="circle arrow icon"
+      />
     </div>
     <div class="container">
       <Item v-for="post in posts" :key="post.id" :post="post">
@@ -40,6 +28,16 @@ import { mapActions, mapState } from 'vuex'
 import gsap from 'gsap'
 
 export default {
+  transition: {
+    css: false,
+    mode: 'out-in',
+    enter(el, onComplete) {
+      gsap.fromTo(el, { delay: 0.1, opacity: 0 }, { opacity: 1, onComplete })
+    },
+    leave(el, onComplete) {
+      gsap.to(el, { opacity: 0, onComplete })
+    },
+  },
   async asyncData({ params, error, store }) {
     const currPage = isNaN(params.page) ? 1 : Number.parseInt(params.page)
     const whitelist = [undefined, 'new', 'top', 'best']
@@ -82,6 +80,9 @@ export default {
   },
   methods: {
     ...mapActions(['toggleDarkMode']),
+    enter(el, onComplete) {
+      gsap.fromTo(el, { delay: 0.2, opacity: 0 }, { opacity: 1, onComplete })
+    },
     refresh() {
       gsap.to(this.$refs.loaderArrow, {
         rotateZ: 1080,
@@ -213,8 +214,12 @@ export default {
 
   &--dark {
     background-color: #1f2129;
+
+    img {
+      filter: invert(1);
+    }
   }
-  svg {
+  img {
     transform: scale(1.75);
   }
 }
